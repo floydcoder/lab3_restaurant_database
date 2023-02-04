9fbd6104b49e5c782fa04fcf25eca8cfab8dc37c
Marco Stevanella <marcostevanella.canada@gmail.com>
2023-02-04 11:52:10 -0500

    add database connection to project, updated the gitIgnore file

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const employeeRouter = require('./routes/EmployeeRoutes.js');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('PASTE_YOUR_CONNECTION_STRING_HERE', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(success => {
-  console.log('Success Mongodb connection')
-}).catch(err => {
-  console.log('Error Mongodb connection')
-});
+mongoose
+  .connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((success) => {
+    console.log('Success Mongodb connection');
+  })
+  .catch((err) => {
+    console.log('Error Mongodb connection');
+  });
 
 app.use(employeeRouter);
 
-app.listen(8081, () => { console.log('Server is running...') });
+app.listen(8081, () => {
+  console.log('Server is running...');
+});
